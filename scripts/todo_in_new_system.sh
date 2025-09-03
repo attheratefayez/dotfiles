94f37acad016c9b22c400ab919430b7f2f4eabe0
attheratefayez <faizurrahman.fayez@gmail.com>
2025-09-03 20:53:54 +0600

    added installation commands for nvidia-container toolkit

--- scripts/todo_in_new_system.sh
+++ scripts/todo_in_new_system.sh
@@ -51,4 +51,19 @@ apt install ghostty
 update-alternatives --install  /usr/bin/x-terminal-emulator x-terminal-emulator $(which alacritty) 50
 update-alternatives --set x-terminal-emulator $(which alacritty)
 
+# install nvidia-container toolkit
+#
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | \
+gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
+apt update
+
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1 \
+&& apt install -y \
+      nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
