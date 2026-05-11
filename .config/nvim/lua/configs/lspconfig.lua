c818084ac28d8dc33a59ab5f6ab8ac61395af1c2
fayez <faizurrahman.fayez@gmail.com>
2026-05-11 19:26:39 +0600

    trying out nvim-lite

--- .config/nvim/lua/configs/lspconfig.lua
+++
@@ -1,8 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "clangd", "pyright" }
-vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
---
-require "configs.custom_pyright"
