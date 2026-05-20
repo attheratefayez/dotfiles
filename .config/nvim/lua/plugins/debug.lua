30e519042985c5219ea33097f502e81a9fa01c0f
fayez <faizurrahman.fayez@gmail.com>
2026-05-20 23:16:52 +0600

    refactored and addred keybinds, left, down, right
    left - step-out
    down - step-over
    right - step-into

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -7,35 +7,24 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 local gh = require('vim_pack_nvim').gh
 
-local debug_plugins = {
-  'mfussenegger/nvim-dap',
-  'rcarriga/nvim-dap-ui',
-  'nvim-neotest/nvim-nio',
---  'mason-org/mason.nvim',
-  'jay-babu/mason-nvim-dap.nvim',
-  'mfussenegger/nvim-dap-python',
-}
-
-local addables = {}
-for _, val in ipairs(debug_plugins) do
-  table.insert(addables, {
-    src = gh(val),
-  })
-end
-
-vim.pack.add(addables, { load = false })
+vim.pack.add({ { src = gh 'mfussenegger/nvim-dap' } }, { load = false })
+vim.pack.add({ { src = gh 'rcarriga/nvim-dap-ui' } }, { load = false })
+vim.pack.add({ { src = gh 'nvim-neotest/nvim-nio' } }, { load = false })
+vim.pack.add({ { src = gh 'jay-babu/mason-nvim-dap.nvim' } }, { load = false })
+vim.pack.add({ { src = gh 'mfussenegger/nvim-dap-python' } }, { load = false })
 
 local function load_debug()
-  for _, val in ipairs(debug_plugins) do
-    local plug_name = val:match '.+/(.+)'
-    vim.cmd.packadd { plug_name }
-  end
+  vim.cmd.packadd { 'mfussenegger/nvim-dap' }
+  vim.cmd.packadd { 'rcarriga/nvim-dap-ui' }
+  vim.cmd.packadd { 'nvim-neotest/nvim-nio' }
+  vim.cmd.packadd { 'jay-babu/mason-nvim-dap.nvim' }
+  vim.cmd.packadd { 'mfussenegger/nvim-dap-python' }
 
   -- Basic debugging keymaps, feel free to change to your liking!
   vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'Debug: Start/Continue' })
-  vim.keymap.set('n', '<F1>', function() require('dap').step_into() end, { desc = 'Debug: Step Into' })
-  vim.keymap.set('n', '<F2>', function() require('dap').step_over() end, { desc = 'Debug: Step Over' })
-  vim.keymap.set('n', '<F3>', function() require('dap').step_out() end, { desc = 'Debug: Step Out' })
+  vim.keymap.set('n', '<Right>', function() require('dap').step_into() end, { desc = 'Debug: Step Into' })
+  vim.keymap.set('n', '<Down>', function() require('dap').step_over() end, { desc = 'Debug: Step Over' })
+  vim.keymap.set('n', '<Left>', function() require('dap').step_out() end, { desc = 'Debug: Step Out' })
   vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end, { desc = 'Debug: Toggle Breakpoint' })
   vim.keymap.set('n', '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, { desc = 'Debug: Set Breakpoint' })
   -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
